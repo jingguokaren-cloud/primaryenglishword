c169c408e5c89a87e53010325d2054904516e709
jingguokaren-cloud <jingguokaren@gmail.com>
2026-05-29 13:03:29 +0800

    Add files via upload

---
+++ 小学教材打字练习/words.js
@@ -0,0 +1,365 @@
+// 小学教材（三下）单词数据
+
+const nceWords = {
+  1: {
+    title: "三下 Unit 1",
+    words: [
+      { en: "dress", cn: "穿衣；连衣裙" },
+      { en: "brush", cn: "刷" },
+      { en: "tooth", cn: "牙齿（复数为teeth）" },
+      { en: "wash", cn: "洗" },
+      { en: "breakfast", cn: "早饭" },
+      { en: "after", cn: "在……之后" },
+      { en: "forget", cn: "忘记" },
+      { en: "table", cn: "桌子" },
+      { en: "late", cn: "迟到" },
+      { en: "quick", cn: "快的" },
+      { en: "go", cn: "去" },
+      { en: "Saturday", cn: "星期六" },
+      { en: "today", cn: "今天" }
+    ]
+  },
+  2: {
+    title: "三下 Unit 2",
+    words: [
+      { en: "ten", cn: "十" },
+      { en: "o'clock", cn: "……点钟" },
+      { en: "half", cn: "一半" },
+      { en: "past", cn: "过了（时间）；过去的" },
+      { en: "thirty", cn: "三十" },
+      { en: "home", cn: "家" },
+      { en: "school", cn: "学校" },
+      { en: "bed", cn: "床" },
+      { en: "time", cn: "时间" },
+      { en: "activity", cn: "活动" },
+      { en: "no", cn: "不; 没有" },
+      { en: "next", cn: "紧接着" },
+      { en: "eight", cn: "八" },
+      { en: "hurry", cn: "赶快" },
+      { en: "dream", cn: "梦" },
+      { en: "eleven", cn: "十一" },
+      { en: "twelve", cn: "十二" }
+    ]
+  },
+  3: {
+    title: "三下 Unit 3",
+    words: [
+      { en: "plan", cn: "计划" },
+      { en: "twenty", cn: "二十" },
+      { en: "forty", cn: "四十" },
+      { en: "fifty", cn: "五十" },
+      { en: "dinner", cn: "晚饭" },
+      { en: "book", cn: "书" },
+      { en: "sport", cn: "运动" },
+      { en: "TV", cn: "电视 (=television)" },
+      { en: "club", cn: "社团" },
+      { en: "at", cn: "在……" },
+      { en: "maybe", cn: "也许" },
+      { en: "think", cn: "想，认为" },
+      { en: "about", cn: "关于" },
+      { en: "pack", cn: "收拾" },
+      { en: "schoolbag", cn: "书包" },
+      { en: "hour", cn: "小时" },
+      { en: "every", cn: "每个" },
+      { en: "day", cn: "一天" },
+      { en: "take", cn: "乘坐; 需要……（时间）" },
+      { en: "sleep", cn: "睡觉" },
+      { en: "find", cn: "找到" },
+      { en: "Sunday", cn: "星期日" },
+      { en: "child", cn: "孩子（复数为children）" },
+      { en: "mother", cn: "母亲" },
+      { en: "father", cn: "父亲" },
+      { en: "new", cn: "新的" },
+      { en: "year", cn: "年" }
+    ]
+  },
+  4: {
+    title: "三下 Unit 4",
+    words: [
+      { en: "join", cn: "加入" },
+      { en: "sure", cn: "当然" },
+      { en: "love", cn: "愿意，喜爱" },
+      { en: "football", cn: "足球" },
+      { en: "just", cn: "只是; 正好" },
+      { en: "thing", cn: "东西，事物" },
+      { en: "fun", cn: "乐趣; 有趣的" },
+      { en: "ask", cn: "邀请；询问" },
+      { en: "her", cn: "她；她的" },
+      { en: "idea", cn: "主意" },
+      { en: "eat", cn: "吃" },
+      { en: "together", cn: "一起" }
+    ]
+  },
+  5: {
+    title: "三下 Unit 5",
+    words: [
+      { en: "run", cn: "跑" },
+      { en: "best", cn: "最好；最好的" },
+      { en: "rule", cn: "规则" },
+      { en: "turn", cn: "（依次轮到的）机会；转动" },
+      { en: "speak", cn: "说话" },
+      { en: "give", cn: "给" },
+      { en: "away", cn: "离开" },
+      { en: "must", cn: "必须" },
+      { en: "need", cn: "需要" },
+      { en: "top", cn: "（位置、级别或程度）最高" },
+      { en: "talk", cn: "说话" },
+      { en: "exercise", cn: "练习；锻炼" },
+      { en: "clean", cn: "干净的；打扫" },
+      { en: "arrive", cn: "到达" }
+    ]
+  },
+  6: {
+    title: "三下 Unit 6",
+    words: [
+      { en: "stand", cn: "站立" },
+      { en: "line", cn: "队伍，行列" },
+      { en: "lead", cn: "带领" },
+      { en: "way", cn: "路线；方法" },
+      { en: "carry", cn: "搬运" },
+      { en: "food", cn: "食物" },
+      { en: "out", cn: "（拿）出" },
+      { en: "paper", cn: "试卷；纸" },
+      { en: "lunch", cn: "午餐" },
+      { en: "light", cn: "灯；轻的" },
+      { en: "blackboard", cn: "黑板" },
+      { en: "door", cn: "门" },
+      { en: "plant", cn: "植物" },
+      { en: "Monday", cn: "星期一" },
+      { en: "April", cn: "四月" },
+      { en: "he", cn: "他" },
+      { en: "such", cn: "非常；这样的" },
+      { en: "should", cn: "应该" },
+      { en: "some", cn: "一些" },
+      { en: "tomato", cn: "西红柿" },
+      { en: "work", cn: "工作" },
+      { en: "so", cn: "很；所以" },
+      { en: "PE", cn: "体育 (=physical education)" },
+      { en: "maths", cn: "数学" }
+    ]
+  },
+  7: {
+    title: "三下 Unit 7",
+    words: [
+      { en: "hold", cn: "使保持; 拿着" },
+      { en: "excuse", cn: "原谅" },
+      { en: "voice", cn: "声音" },
+      { en: "down", cn: "减弱; 向下" },
+      { en: "or", cn: "或者" },
+      { en: "drink", cn: "喝" },
+      { en: "we", cn: "我们" },
+      { en: "polite", cn: "礼貌的" },
+      { en: "there", cn: "那里" },
+      { en: "sorry", cn: "对不起" },
+      { en: "toilet", cn: "厕所" },
+      { en: "knock", cn: "敲" },
+      { en: "off", cn: "关闭" },
+      { en: "water", cn: "水" },
+      { en: "block", cn: "挡住" }
+    ]
+  },
+  8: {
+    title: "三下 Unit 8",
+    words: [
+      { en: "window", cn: "窗户" },
+      { en: "fan", cn: "风扇；爱好者" },
+      { en: "close", cn: "关闭" },
+      { en: "piano", cn: "钢琴" },
+      { en: "desk", cn: "书桌" },
+      { en: "computer", cn: "电脑" },
+      { en: "use", cn: "使用" },
+      { en: "care", cn: "小心; 照料" },
+      { en: "chair", cn: "椅子" },
+      { en: "classroom", cn: "教室" },
+      { en: "other", cn: "他人; 其他的" },
+      { en: "library", cn: "图书馆" },
+      { en: "anyone", cn: "任何人" },
+      { en: "before", cn: "在……之前" },
+      { en: "place", cn: "地点" }
+    ]
+  },
+  9: {
+    title: "三下 Review",
+    words: [
+      { en: "litter", cn: "乱扔；垃圾" },
+      { en: "road", cn: "道路" },
+      { en: "sign", cn: "标志" },
+      { en: "maybe", cn: "也许" }
+    ]
+  },
+  11: {
+    title: "三上 Unit 1",
+    words: [
+      { en: "me", cn: "我" },
+      { en: "it", cn: "它" },
+      { en: "be", cn: "是（am/is/are）" },
+      { en: "on", cn: "在…上面" },
+      { en: "this", cn: "这，这个" },
+      { en: "ruler", cn: "尺子" },
+      { en: "you", cn: "你，你们" },
+      { en: "I", cn: "我" },
+      { en: "thank", cn: "感谢，向表示感谢" },
+      { en: "nice", cn: "令人愉快的" },
+      { en: "to", cn: "到，向，往" },
+      { en: "meet", cn: "结识；遇见" },
+      { en: "too", cn: "也" },
+      { en: "hi", cn: "嗨，喂，你好" },
+      { en: "but", cn: "但，但是" }
+    ]
+  },
+  12: {
+    title: "三上 Unit 2",
+    words: [
+      { en: "chocolate", cn: "巧克力" },
+      { en: "coffee", cn: "咖啡" },
+      { en: "Chinese", cn: "中文" },
+      { en: "sofa", cn: "沙发" },
+      { en: "hamburger", cn: "汉堡" },
+      { en: "cookie", cn: "饼干" },
+      { en: "salad", cn: "沙拉" },
+      { en: "what", cn: "什么" },
+      { en: "in", cn: "在……里面" },
+      { en: "English", cn: "英语" },
+      { en: "know", cn: "知道；认识" },
+      { en: "yes", cn: "是，对" },
+      { en: "can", cn: "能, 可以" },
+      { en: "get", cn: "得到" },
+      { en: "a", cn: "一，一个（an）" },
+      { en: "word", cn: "单词" },
+      { en: "guess", cn: "猜，猜测" },
+      { en: "one", cn: "一" },
+      { en: "like", cn: "喜欢" }
+    ]
+  },
+  13: {
+    title: "三上 Unit 3",
+    words: [
+      { en: "good", cn: "好的" },
+      { en: "morning", cn: "早晨；上午" },
+      { en: "your", cn: "你的；你们的" },
+      { en: "name", cn: "名字" },
+      { en: "my", cn: "我的" },
+      { en: "Mr", cn: "先生" },
+      { en: "boy", cn: "男孩" },
+      { en: "and", cn: "和" },
+      { en: "girl", cn: "女孩" },
+      { en: "hello", cn: "你好" },
+      { en: "everyone", cn: "每个人，人人" },
+      { en: "card", cn: "卡片" },
+      { en: "student", cn: "学生" },
+      { en: "want", cn: "想要" },
+      { en: "help", cn: "帮助，帮忙" },
+      { en: "how", cn: "多少；怎样，如何" },
+      { en: "last", cn: "最后" },
+      { en: "of", cn: "…的" },
+      { en: "for", cn: "为了" },
+      { en: "Ms", cn: "女士" },
+      { en: "Miss", cn: "小姐" },
+      { en: "Mrs", cn: "女士，夫人" }
+    ]
+  },
+  14: {
+    title: "三上 Unit 4",
+    words: [
+      { en: "red", cn: "红色" },
+      { en: "yellow", cn: "黄色" },
+      { en: "green", cn: "绿色" },
+      { en: "blue", cn: "蓝色" },
+      { en: "white", cn: "白色" },
+      { en: "colour", cn: "颜色" },
+      { en: "pencil", cn: "铅笔" },
+      { en: "left", cn: "左边" },
+      { en: "hand", cn: "手" },
+      { en: "right", cn: "右边" },
+      { en: "finger", cn: "手指" },
+      { en: "do", cn: "做" },
+      { en: "spell", cn: "拼写" },
+      { en: "that", cn: "那，那个" },
+      { en: "look", cn: "看，瞧，望" },
+      { en: "cool", cn: "酷的，帅的" },
+      { en: "teach", cn: "教" },
+      { en: "first", cn: "第一的；首先；最初" },
+      { en: "OK", cn: "好，可以" }
+    ]
+  },
+  15: {
+    title: "三上 Unit 5",
+    words: [
+      { en: "two", cn: "二" },
+      { en: "three", cn: "三" },
+      { en: "four", cn: "四" },
+      { en: "five", cn: "五" },
+      { en: "six", cn: "六" },
+      { en: "seven", cn: "七" },
+      { en: "nine", cn: "九" },
+      { en: "the", cn: "这，那" },
+      { en: "again", cn: "再，再次" },
+      { en: "step", cn: "步骤" },
+      { en: "letter", cn: "字母" },
+      { en: "from", cn: "从" },
+      { en: "each", cn: "每个" },
+      { en: "put", cn: "放" },
+      { en: "them", cn: "它们" }
+    ]
+  },
+  16: {
+    title: "三上 Unit 6",
+    words: [
+      { en: "face", cn: "脸，面部" },
+      { en: "ear", cn: "耳朵" },
+      { en: "eye", cn: "眼睛" },
+      { en: "nose", cn: "鼻子" },
+      { en: "mouth", cn: "嘴巴" },
+      { en: "its", cn: "它的" },
+      { en: "these", cn: "这些" },
+      { en: "hair", cn: "头发" },
+      { en: "write", cn: "写" },
+      { en: "big", cn: "大的" },
+      { en: "under", cn: "在…下面" },
+      { en: "his", cn: "他的" },
+      { en: "now", cn: "现在" },
+      { en: "head", cn: "头" }
+    ]
+  },
+  17: {
+    title: "三上 Unit 7",
+    words: [
+      { en: "see", cn: "看见；明白" },
+      { en: "nod", cn: "点头" },
+      { en: "keep", cn: "保持" },
+      { en: "quiet", cn: "安静" },
+      { en: "listen", cn: "听" },
+      { en: "teacher", cn: "老师" },
+      { en: "have", cn: "有，拥有" },
+      { en: "up", cn: "向上" },
+      { en: "they", cn: "他们" },
+      { en: "body", cn: "身体" },
+      { en: "friend", cn: "朋友" },
+      { en: "myself", cn: "我自己" },
+      { en: "heart", cn: "心，心脏；内心" }
+    ]
+  },
+  18: {
+    title: "三上 Unit 8",
+    words: [
+      { en: "bird", cn: "鸟" },
+      { en: "horse", cn: "马" },
+      { en: "monkey", cn: "猴子" },
+      { en: "fish", cn: "鱼" },
+      { en: "bear", cn: "熊" },
+      { en: "swim", cn: "游泳" },
+      { en: "walk", cn: "走，走路" },
+      { en: "fly", cn: "飞，飞翔" },
+      { en: "jump", cn: "跳，跳跃" },
+      { en: "arm", cn: "手臂" },
+      { en: "leg", cn: "腿" },
+      { en: "tiger", cn: "老虎" },
+      { en: "then", cn: "然后，接下来" },
+      { en: "let", cn: "让" },
+      { en: "us", cn: "我们" },
+      { en: "move", cn: "移动" },
+      { en: "our", cn: "我们的" },
+      { en: "foot", cn: "脚" }
+    ]
+  }
+};
